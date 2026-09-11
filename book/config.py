035c39ed175250bd2ff4629ff8841e98401c9baa
t01040588614-source <t01040588614@gmail.com>
2026-09-11 17:37:35 +0900

    Update config.py

--- book/config.py
+++ book/config.py
@@ -27,7 +27,7 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = TV7yFDizlFFTLYtttcSh9I4Y0gZT2-a5j65uJJB4938
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
     SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
     SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
